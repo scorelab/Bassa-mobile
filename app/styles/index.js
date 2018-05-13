d6f08863a8060e2fc79d51c011d296ecb600572d
Gayashan Bombuwala <gayashanbc@gmail.com>
2018-05-14 00:38:30 +0530

    Add global styles

--- app/styles/index.js
+++ app/styles/index.js
@@ -1,5 +1,22 @@
-const theme = {
+import { StyleSheet } from 'react-native';
 
+export const theme = {
+  PRIMARY_COLOR: '#51b355',
+  SECONDARY_COLOR: '#002F46',
+  HINT_COLOR: '#00AEFF',
+  TAB_ICON_COLOR: '#0076FF',
+  PRIMARY_STATUS_BAR_COLOR: '#275b38',
+
+  TEXT_COLOR_INVERT: '#FFF',
+  ERROR_TEXT_COLOR: '#cc0000',
 };
 
-export default theme;
+export const globalStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  defaultPadding: {
+    padding: 15,
+  },
+});
+
