8ad09e46b013b785e58dbd287fe06b0ec34608f6
Gayashan Bombuwala <gayashanbc@gmail.com>
2018-05-19 21:48:17 +0530

    Decompose request body preparing from the UserService

--- app/services/userService.js
+++ app/services/userService.js
@@ -1,16 +1,13 @@
 import APIBuilder from '../helpers/APIBuilder';
+import { prepareRequestBody } from '../helpers/utils';
 
 const signIn = (username, password) => {
   const credentials = {
     user_name: username,
     password,
   };
-  let formBody = [];
 
-  Object.keys(credentials).forEach(key => formBody.push(`${encodeURIComponent(key)}=${encodeURIComponent(credentials[key])}`));
-  formBody = formBody.join('&');
-
-  return APIBuilder.API.post('/api/login', formBody);
+  return APIBuilder.API.post('/api/login', prepareRequestBody(credentials));
 };
 
 export default {
