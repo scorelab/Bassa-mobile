e1e43bf7bf04c021fdc1a960eb9144146dac8fd3
Gayashan Bombuwala <gayashanbc@gmail.com>
2018-05-21 22:03:23 +0530

    Add new routes for Downloads and Accounts

--- app/containers/RootNavigator.js
+++ app/containers/RootNavigator.js
@@ -4,20 +4,54 @@ import { createStackNavigator, createDrawerNavigator, createMaterialTopTabNaviga
 // root routes
 import Init from './Init';
 import SignIn from './SignIn';
+import SignUp from './SignUp';
 
 import CustomDrawer from './CustomDrawer';
 
-// downloads routes
+// dashboard routes
 import InProgressDownloads from './InProgressDownloads';
+import CompletedDownloads from './CompletedDownloads';
 
-export const DashboardTabs = createMaterialTopTabNavigator({
+// admin routes
+import Approvals from './Approvals';
+import QuotaUsage from './QuotaUsage';
+
+import { theme } from '../styles';
+
+const tabBarOptions = {
+  labelStyle: {
+    fontSize: 15,
+  },
+  style: {
+    backgroundColor: theme.PRIMARY_COLOR,
+  },
+  scrollEnabled: false,
+};
+
+export const DownloadsTabs = createMaterialTopTabNavigator({
   InProgressDownloads: { screen: InProgressDownloads },
+  CompletedDownloads: { screen: CompletedDownloads },
 
+}, {
+  initialRouteName: 'InProgressDownloads',
+  swipeEnabled: false,
+  tabBarOptions,
+});
+
+export const AccountsTabs = createMaterialTopTabNavigator({
+  Approvals: { screen: Approvals },
+  QuotaUsage: { screen: QuotaUsage },
+
+}, {
+  initialRouteName: 'Approvals',
+  swipeEnabled: false,
+  tabBarOptions,
 });
 
 const Drawer = createDrawerNavigator(
   {
-    MainDrawer: { screen: DashboardTabs },
+    Downloads: { screen: DownloadsTabs },
+    Accounts: { screen: AccountsTabs },
   },
   {
     contentComponent: props => <CustomDrawer {...props} />,
@@ -27,6 +61,7 @@ const Drawer = createDrawerNavigator(
 const AppNavigator = createStackNavigator({
   Init: { screen: Init },
   SignIn: { screen: SignIn },
+  SignUp: { screen: SignUp },
   MainDrawer: { screen: Drawer, navigationOptions: { header: null } },
 });
 
