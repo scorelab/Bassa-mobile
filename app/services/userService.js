73fe2cb2c1d7e033354bcd8d38209016ed126b56
Gayashan Bombuwala <gayashanbc@gmail.com>
2018-05-21 22:05:53 +0530

    Add signUp, getPendingReuests and approveUser methods to UserService

--- app/services/userService.js
+++ app/services/userService.js
@@ -1,15 +1,17 @@
 import APIBuilder from '../helpers/APIBuilder';
 import { prepareRequestBody } from '../helpers/utils';
 
-const signIn = (username, password) => {
-  const credentials = {
-    user_name: username,
-    password,
-  };
+const signIn = credentials => APIBuilder.API.post('/api/login', prepareRequestBody(credentials));
 
-  return APIBuilder.API.post('/api/login', prepareRequestBody(credentials));
-};
+const signUp = userData => APIBuilder.API.post('/api/regularuser', JSON.stringify(userData));
+
+const approveUser = username => APIBuilder.API.post(`/api/user/approve/${username}`);
+
+const getPendingRequests = () => APIBuilder.API.get('/api/user/requests');
 
 export default {
   signIn,
+  signUp,
+  approveUser,
+  getPendingRequests,
 };
