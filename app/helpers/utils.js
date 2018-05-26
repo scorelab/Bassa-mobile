76f4e675757536c35762775f92c9d0519b79d838
Gayashan Bombuwala <gayashanbc@gmail.com>
2018-05-26 18:02:29 +0530

    Add formatBytes method to utils

--- app/helpers/utils.js
+++ app/helpers/utils.js
@@ -6,3 +6,15 @@ export const prepareRequestBody = (requestParams = {}) => {
 
   return formBody;
 };
+
+export const formatBytes = (bytes) => {
+  if (bytes === 0) {
+    return '0 Bytes';
+  }
+  const k = 1000;
+  const dm = 3;
+  const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+  
+  return `${parseFloat((bytes / (k ** i)).toFixed(dm))} ${sizes[i]}`;
+};
