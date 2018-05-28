2d5901610f23683d904075f1ed968abf1ca3e97e
Gayashan Bombuwala <gayashanbc@gmail.com>
2018-05-28 21:41:33 +0530

    Add removeDownload() method to DownloadService

--- app/services/downloadService.js
+++ app/services/downloadService.js
@@ -2,9 +2,12 @@ import APIBuilder from '../helpers/APIBuilder';
 
 const getAllDownloads = () => APIBuilder.API.get('/api/downloads/1');
 
+const removeDownload = downloadId => APIBuilder.API.delete(`/api/download/${downloadId}`);
+
 const getDownloadsForUser = () => APIBuilder.API.get('/api/user/downloads/1');
 
 export default {
   getDownloadsForUser,
+  removeDownload,
   getAllDownloads,
 };
