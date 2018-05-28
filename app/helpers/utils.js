99788f282577c1a128c146cd35c1af9dcfa23862
Gayashan Bombuwala <gayashanbc@gmail.com>
2018-05-28 21:38:11 +0530

    Replace exponent operator with Math.pow() to avoid build issues

--- app/helpers/utils.js
+++ app/helpers/utils.js
@@ -16,5 +16,5 @@ export const formatBytes = (bytes) => {
   const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
   const i = Math.floor(Math.log(bytes) / Math.log(k));
   
-  return `${parseFloat((bytes / (k ** i)).toFixed(dm))} ${sizes[i]}`;
+  return `${parseFloat((bytes / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`;
 };
