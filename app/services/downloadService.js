1f6927a059d25fd0e051ca3fe40c45ca9b76e43b
Gayashan Bombuwala <gayashanbc@gmail.com>
2018-06-02 20:52:29 +0530

    Implement addDownload() method in  the DownloadService

--- app/services/downloadService.js
+++ app/services/downloadService.js
@@ -6,8 +6,11 @@ const removeDownload = downloadId => APIBuilder.API.delete(`/api/download/${down
 
 const getDownloadsForUser = () => APIBuilder.API.get('/api/user/downloads/1');
 
+const addDownload = downloadLink => APIBuilder.API.post('/api/download', JSON.stringify(downloadLink));
+
 export default {
   getDownloadsForUser,
   removeDownload,
   getAllDownloads,
+  addDownload,
 };
