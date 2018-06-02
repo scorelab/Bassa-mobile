366a262572e87b83b619f5011b71b68e72b7aef6
Gayashan Bombuwala <gayashanbc@gmail.com>
2018-06-02 20:54:25 +0530

    Add validations for download links

--- app/containers/InProgressDownloads/index.js
+++ app/containers/InProgressDownloads/index.js
@@ -130,16 +130,29 @@ class InProgressDownloads extends Component {
     );
   }
 
+  isValidLink(link) {
+    const urlvalidatorRegex = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/[^\s]*)?$/i;
+    const magnetvalidatorRegex = /magnet:\?xt=/i;
+    if (urlvalidatorRegex.test(link) || link.match(magnetvalidatorRegex) !== null) {
+      return true;
+    }
+    return false;
+  }
+
   async onDownloadSubmit(link) {
-    this.setState({
-      isPromptVisible: false,
-    });
-    try {
-      await DownloadService.addDownload({ link });
-      this.toastRef.current.show('New download added');
-      this.fetchActiveDownloads();
-    } catch (error) {
-      Alert.alert('Error', 'An error occured while adding the download');
+    if (this.isValidLink(link)) {
+      this.setState({
+        isPromptVisible: false,
+      });
+      try {
+        await DownloadService.addDownload({ link });
+        this.toastRef.current.show('New download added');
+        this.fetchActiveDownloads();
+      } catch (error) {
+        Alert.alert('Error', 'An error occured while adding the download');
+      }
+    } else {
+      this.toastRef.current.show('Invalid download link');
     }
   }
 
