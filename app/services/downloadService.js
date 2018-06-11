2a78beaf1fb22b1d9c6a903d3ff3c7690be039fd
Gayashan Bombuwala <gayashanbc@gmail.com>
2018-06-11 21:29:06 +0530

    Add startAllDownloads() killAllDownloads() to downloadService

--- app/services/downloadService.js
+++ app/services/downloadService.js
@@ -9,11 +9,25 @@ const getDownloadsForUser = () => APIBuilder.API.get('/api/user/downloads/1');
 
 const getQuotaUsages = () => APIBuilder.API.get('/api/user/heavy');
 
+const startAllDownloads = () => APIBuilder.API.get('/download/start', {
+  headers: {
+    key: APIConstants.KEY,
+  },
+});
+
+const killAllDownloads = () => APIBuilder.API.get('/download/kill', {
+  headers: {
+    key: APIConstants.KEY,
+  },
+});
+
 const addDownload = downloadLink => APIBuilder.API.post('/api/download', JSON.stringify(downloadLink));
 
 export default {
   getDownloadsForUser,
   removeDownload,
+  startAllDownloads,
+  killAllDownloads,
   getQuotaUsages,
   getAllDownloads,
   addDownload,
