8d8765ad704f389ad98fd37d3b31554c9b4ba1ea
Gayashan Bombuwala <gayashanbc@gmail.com>
2018-06-24 14:15:15 +0530

    Add showPushNotification() method to utils.js

--- app/helpers/utils.js
+++ app/helpers/utils.js
@@ -1,3 +1,7 @@
+import PushNotification from 'react-native-push-notification';
+
+import { theme } from '../styles';
+
 export const prepareRequestBody = (requestParams = {}) => {
   let formBody = [];
 
@@ -15,6 +19,19 @@ export const formatBytes = (bytes) => {
   const dm = 3;
   const sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
   const i = Math.floor(Math.log(bytes) / Math.log(k));
-  
+
   return `${parseFloat((bytes / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`;
+};
+
+export const showPushNotification = (title, message, description) => {
+  PushNotification.localNotification({
+    bigText: description,
+    color: theme.PRIMARY_STATUS_BAR_COLOR,
+    vibration: 300,
+    ongoing: false,
+    title,
+    message,
+    soundName: 'default',
+    number: '10',
+  });
 };
