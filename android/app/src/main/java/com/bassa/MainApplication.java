9121dbf111439bd142e5534c1b7cf0e3ae9a3201
Gayashan Bombuwala <gayashanbc@gmail.com>
2018-06-24 14:14:34 +0530

    Install and configure push notifications library

--- android/app/src/main/java/com/bassa/MainApplication.java
+++ android/app/src/main/java/com/bassa/MainApplication.java
@@ -3,6 +3,7 @@ package com.bassa;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 import com.horcrux.svg.SvgPackage;
 import com.react.rnspinkit.RNSpinkitPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativePushNotificationPackage(),
             new SvgPackage(),
             new RNSpinkitPackage()
       );
