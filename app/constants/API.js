e0d2d882b77691cdd93b2ccad40056208423f8da
Łukasz Myśliwiec <lmysliwiec10@gmail.com>
2018-10-28 19:09:39 +0100

    Put back proper HOST_URL

--- app/constants/API.js
+++ app/constants/API.js
@@ -1,6 +1,6 @@
 // TODO: Remove this, as APIConstants is available through Redux store
 const APIConstants = {
-  HOST_URL: 'http://192.168.16.130',
+  HOST_URL: 'http://10.0.3.2',
   HOST_PORT: 5000,
   KEY: '123456789',
 };
