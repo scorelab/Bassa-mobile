d407a8e83c60c990967067f7cbe5962736e31d75
Rohan Singh <rohan.clevduct@gmail.com>
2018-10-30 16:43:13 +0530

    updated constantsActions.js
    Removed merged conflicts

--- app/actions/constantsActions.js
+++ app/actions/constantsActions.js
@@ -1,23 +1,6 @@
 // @flow
 import { constantsActions } from './types';
 
-<<<<<<< HEAD
-export const getHostUrl = () => {
-  return {
-    type: constantsActions.GET_HOST_URL,
-  };
-};
-
-export const getHostPort = () => {
-  return {
-    type: constantsActions.GET_HOST_PORT,
-  };
-};
-
-export const getKey = () => {
-  return {
-    type: constantsActions.GET_KEY,
-=======
 export const setKey = key => {
   return {
     type: constantsActions.SET_KEY,
@@ -29,7 +12,6 @@ export const setHostPort = port => {
   return {
     type: constantsActions.SET_HOST_PORT,
     payload: port,
->>>>>>> HEAD@{1}
   };
 };
 
@@ -40,21 +22,6 @@ export const setHostUrl = url => {
   };
 };
 
-<<<<<<< HEAD
-export const setHostPort = port => {
-  return {
-    type: constantsActions.SET_HOST_PORT,
-    payload: port,
-  };
-};
-
-export const setKey = key => {
-  return {
-    type: constantsActions.SET_KEY,
-    payload: key,
-  };
-};
-=======
 export const getKey = () => {
   return {
     type: constantsActions.GET_KEY,
@@ -66,10 +33,3 @@ export const getHostPort = () => {
     type: constantsActions.GET_HOST_PORT,
   };
 };
-
-export const getHostUrl = () => {
-  return {
-    type: constantsActions.GET_HOST_URL,
-  };
-};
->>>>>>> HEAD@{1}
