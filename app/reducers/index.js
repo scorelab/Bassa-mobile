c559ff56a433285b17ce51e6c0d3b90739c7d69c
anuragparida <anuragparida37@gmail.com>
2018-10-31 04:34:52 +0530

    updated reducers/index.js

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,18 +1,20 @@
-import { combineReducers } from 'redux';
-import { createNavigationReducer } from 'react-navigation-redux-helpers';
+import { combineReducers } from "redux";
+import { createNavigationReducer } from "react-navigation-redux-helpers";
 
-import AppNavigator from '../containers/RootNavigator';
-import appReducer from './appReducer';
-import userReducer from './userReducer';
-import downloadsReducer from './downloadsReducer';
+import getSetReducer from "./getsetConstReducer";
+import AppNavigator from "../containers/RootNavigator";
+import appReducer from "./appReducer";
+import userReducer from "./userReducer";
+import downloadsReducer from "./downloadsReducer";
 
 const navReducer = createNavigationReducer(AppNavigator);
 
 const rootReducer = combineReducers({
+  constants: getSetReducer,
   app: appReducer,
   user: userReducer,
   nav: navReducer,
-  downloads: downloadsReducer,
+  downloads: downloadsReducer
 });
 
 export default rootReducer;
